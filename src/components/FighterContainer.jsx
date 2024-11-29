084e90d1cbc88b5aa41503f26887c0a2c0f679e8
albertoarr <albertoarranzromano@gmail.com>
2024-11-29 19:27:59 +0000

    final

--- src/components/FighterContainer.jsx
+++ src/components/FighterContainer.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import './FighterContainer.css'
+import { IonImg, IonTitle } from '@ionic/react'
 export default function FighterContainer({fighter}) {
   return (
     <div>
       <div className="fighter-container">
         <div className="dice-container">
-          <img src="dado.png" alt="dado" />
-          <h1>{fighter.dice}</h1>
+          <IonImg className='diceImg' src="dado.png" alt="dado" />
+          <IonTitle className='dice-text'>{fighter.dice}</IonTitle>
         </div>
-        <h2>{fighter.name}</h2>{" "}
-        <img src={fighter.image} alt="Julio" />
+        <IonTitle className='fighter-name'>{fighter.name}</IonTitle>{" "}
+        <IonImg className='fighter-img' src={fighter.image} alt={fighter.name} />
         <div className="health-container">
           {Array.from({ length: fighter.health }).map((_, index) => (
-            <img key={index} src="vida.png" alt="vida" />
+            <IonImg className='health-img' key={index} src="vida.png" alt="vida" />
           ))}
         </div>
-        <h3>Pereza: {fighter.pereza}</h3>
-        <h3>Maldad: {fighter.maldad}</h3>
-        <h3>Chepa: {fighter.chepa}</h3>
+        <IonTitle className='fighter-stats'>Pereza: {fighter.pereza}</IonTitle>
+        <IonTitle className='fighter-stats'>Maldad: {fighter.maldad}</IonTitle>
+        <IonTitle className='fighter-stats'>Chepa: {fighter.chepa}</IonTitle>
       </div>
     </div>
   )
